d0101aaaa2a9ff0bea71835d8d247873f207bd63
lebrancconvas <lebrancconvas@gmail.com>
2022-04-05 02:17:39 +0700

    ✨ Fill Background Color and Draw Log Text.

--- main.go
+++ main.go
@@ -1,39 +1,42 @@
-package main;
+package main
 
 import (
-	"fmt"
+	"log"
+    "image/color"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 );
 
-// Game implements ebiten.Game interface.
+// Create Game Struct 
 type Game struct{};
 
-// Update proceeds the game state.
-// Update is called every tick (1/60 [s] by default).
+// Update
 func (g *Game) Update() error {
-    // Write your game's logical update.
-    return nil;
-};
+	return nil;
+}
 
-// Draw draws the game screen.
-// Draw is called every frame (typically 1/60[s] for 60Hz display).
+
+// Draw
 func (g *Game) Draw(screen *ebiten.Image) {
-    // Write your game's rendering.
-};
 
-// Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
-// If you don't have to adjust the screen size with the outside size, just return a fixed size.
+    // Fill Screen. 
+    screen.Fill(color.RGBA{0xff, 0, 0xff, 0xa3});
+
+    // Text Log. 
+    messageText := "Debug Log Test.";
+	ebitenutil.DebugPrint(screen, messageText);
+}
+
+
+// Layout 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-    return 320, 240;
+	return 320, 240;
 }
 
 func main() {
-    game := &Game{}
-    // Specify the window size as you like. Here, a doubled size is specified.
-    ebiten.SetWindowSize(640, 480);
-    ebiten.SetWindowTitle("The Fake Master: Spatial");
-    // Call ebiten.RunGame to start your game loop.
-    if err := ebiten.RunGame(game); err != nil {
-        fmt.Println(err);
-    }
-};
+	ebiten.SetWindowSize(640, 480);
+	ebiten.SetWindowTitle("Test Ebiten GUI");
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err);
+	}
+}
